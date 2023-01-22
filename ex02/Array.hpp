538107a376b8c74504819e1c38dccd6451dbbd10
El mehdi Bouabbadi <ebouabba@e2r2p1.1337.ma>
2023-01-22 18:54:25 +0100

    second_push

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -5,5 +5,43 @@
 
 template <class T>
 
+class Array
+{
+private:
+    T *arr;
+    int length;
+
+public:
+    Array()
+    {
+        this->arr = nullptr;
+    }
+    Array(unsigned int n)
+    {
+        this->arr = new T(n);
+    }
+    Array(const Array &c)
+    {
+        this->arr = nullptr;
+        *this = c;
+    }
+    Array &operator=(const Array &c)
+    {
+        this->length = c.length;
+        if (this != &c)
+        {
+            this->~Array();
+            this->arr = new T(c.length);
+            for (size_t i = 0; i < count; i++)
+                this->arr[i] = c.arr[i];
+        }
+        return (*this)
+    }
+    ~Array()
+    {
+        if (this->arr)
+            delete[] this->arr;
+    }
+};
 
 #endif
